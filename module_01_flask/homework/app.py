ea84af5c2c2fc3ff6f6febad83a5036400e0471c
UserPC <meliorator50@mail.ru>
2023-10-10 20:12:38 +0300

    temp1

--- module_01_flask/homework/app.py
+++ module_01_flask/homework/app.py
@@ -1,42 +1,58 @@
 import datetime
+from datetime import timedelta
 from flask import Flask
+import random
+import os
+import re
 
 app = Flask(__name__)
 
 
 @app.route('/hello_world')
 def test_function():
-    pass
+    print('Привет, мир!')
 
 
 @app.route('/cars')
 def test_function():
-    pass
+    print('Chevrolet, Renault, Ford, Lada')
 
 
 @app.route('/cats')
 def test_function():
-    pass
-
+    cats = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
+    print(random.choice(cats))
 
 @app.route('/get_time/now')
 def test_function():
-    pass
+    current_time = datetime.datetime.now()
+    print(f'Точное время: {current_time}')
 
 
 @app.route('/get_time/future')
 def test_function():
-    pass
+    current_time_after_hour = datetime.datetime.now() + timedelta(hours=1)
+    print(f'Точное время через час будет {current_time_after_hour}')
 
 
 @app.route('/get_random_word')
 def test_function():
-    pass
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
 
+    with open(BOOK_FILE) as book:
+        text = book.read()
+        result = re.split(r'[\s]', text)
+        print(random.choice(result))
 
+visits = 0
 @app.route('/counter')
 def test_function():
-    pass
+    global visits
+    visits += 1
+    print(f'Страницу посещали {visits} раз')
+
+
 
 
 if __name__ == '__main__':
