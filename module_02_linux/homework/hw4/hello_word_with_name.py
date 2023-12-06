1cf5a391429169630c31a0b7df424320fbeb5815
marina <meliorator50@mail.ru>
2023-12-06 07:00:13 +0300

    temp07

--- module_02_linux/homework/hw4/hello_word_with_name.py
+++ module_02_linux/homework/hw4/hello_word_with_name.py
@@ -12,20 +12,13 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-weekdays = ["понедельника", "вторника", "среды", "четверга", "пятницы", "субботы", "воскресенья"]
-
-@app.route('/hello-world/')
-def hello_world():
-    today_weekday = weekdays[datetime.today().weekday()]
-    return f"Привет, Петя. Хорошего {today_weekday}!" if datetime.today().weekday() in (0, 1, 3, 6) \
-        else f"Привет, Петя. Хорошей {today_weekday}!"
-
 # TODO правильный код будет таким
-# weekdays = ["понедельника", "вторника", "среды", "четверга", "пятницы", "субботы", "воскресенья"]
-# @app.route('/hello-world/<name>')
-# def hello_world(name):
-#     today_weekday = weekdays[datetime.today().weekday()]
-#     return f"Привет, {name}. Хорошего {today_weekday}!" if today_weekday in (0, 1, 3, 6) else f"Привет, {name}. Хорошей {today_weekday}!"
+weekdays = ["понедельника", "вторника", "среды", "четверга", "пятницы", "субботы", "воскресенья"]
+@app.route('/hello-world/<name>')
+def hello_world(name):
+     today_weekday = weekdays[datetime.today().weekday()]
+     return f"Привет, {name}. Хорошего {today_weekday}!" if datetime.today().weekday() in (0, 1, 3, 6) \
+         else f"Привет, {name}. Хорошей {today_weekday}!"
 # TODO попробуйте его запустить и показать наглядно как вы это сделали,
 # TODO процесс запуска описывал в сообщении в ЛМС от 3 декабря 2023 г. 16:31
 
