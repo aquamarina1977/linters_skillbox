0f3660f3ca97aa293ed59ba5fe6185c8289f3bc8
User <housechel@mail.ru>
2024-03-22 14:04:14 +0300

    Module 06 - 07 Corrected

--- module_06_debugging_begin/homework/hw7/binary_tree_walk.py
+++ module_06_debugging_begin/homework/hw7/binary_tree_walk.py
@@ -67,8 +67,31 @@ def get_tree(max_depth: int, level: int = 1) -> Optional[BinaryTreeNode]:
     node_right = get_tree(max_depth - 1, level=level + 1)
     node = BinaryTreeNode(val=next(counter), left=node_left, right=node_right)
 
+
     return node
 
+# TODO можно такой вариант логики использовать
+# def restore_tree(walk_log_path: str) -> BinaryTreeNode:
+#     import re
+#
+#     tree: dict[int, BinaryTreeNode] = {}
+#     PATTERN: str = r"\d+"
+#
+#     with open(walk_log_path) as log:
+#         for line in log.readlines():
+#             values: list[int] = list(map(int, re.findall(PATTERN, line)))
+#             if "INFO" in line and values[0] not in tree:
+#                 tree[values[0]] = BinaryTreeNode(val=values[0])
+#             elif "left" in line:
+#                 left = BinaryTreeNode(val=values[1])
+#                 tree[values[1]] = left
+#                 tree[values[0]].left = tree[values[1]]
+#             elif "right" in line:
+#                 right = BinaryTreeNode(val=values[1])
+#                 tree[values[1]] = right
+#                 tree[values[0]].right = tree[values[1]]
+#
+#     return next(iter(tree.values()))
 
 def restore_tree(path_to_log_file: str = "walk_log_1.txt") -> BinaryTreeNode:
     nodes = {}
