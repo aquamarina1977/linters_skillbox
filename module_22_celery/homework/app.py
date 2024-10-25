1790f101b1eb2fe075861df1d37bef8c41f77f46
marina <meliorator50@mail.ru>
2024-10-25 19:28:39 +0300

    temp03

--- module_22_celery/homework/app.py
+++ module_22_celery/homework/app.py
@@ -1,23 +1,19 @@
-from celery.result import AsyncResult
 from flask import Flask, request, jsonify
+from celery import Celery, group
 from celery.result import GroupResult
-from celery import group
-from celery import Celery
 from celery.schedules import crontab
 from image import blur_image
 from mail import send_email
 from config import SMTP_USER, SMTP_HOST, SMTP_PASSWORD, SMTP_PORT
 
 app = Flask(__name__)
+
 app.config.update(
     broker_url='redis://localhost:6381/0',
     result_backend='redis://localhost:6381/0',
     task_result_expires=3600,
     result_persistent=True,
-    smtp_user=SMTP_USER,
-    smtp_host=SMTP_HOST,
-    smtp_password=SMTP_PASSWORD,
-    smtp_port=SMTP_PORT
+    redis_max_connections=20
 )
 
 def make_celery(app):
@@ -31,7 +27,7 @@ def make_celery(app):
         'send-weekly-newsletter': {
             'task': 'send_weekly_email',
             'schedule': crontab(day_of_week=0, hour=8, minute=0),
-            'args': ('example@example.com',)
+            'args': ('seurat298@gmail.com', 'meliorator50@mail.ru')
         }
     }
     return celery
@@ -47,19 +43,22 @@ def blur_images():
     )()
     return jsonify({'task_id': task_group.id}), 202
 
-
 @app.route('/status/<task_id>', methods=['GET'])
 def task_status(task_id):
-    group_result = GroupResult.restore(task_id)
+    group_result = GroupResult.restore(task_id, backend=celery.backend)
     if not group_result:
         return jsonify({'status': 'Task ID not found'}), 404
-    completed_tasks = group_result.completed_count()
-    total_tasks = len(group_result)
-    return jsonify({
-        'status': 'in_progress' if not group_result.ready() else 'completed',
-        'progress': f'{completed_tasks}/{total_tasks}'
-    })
-
+    if group_result.ready():
+        return jsonify({
+            'status': 'completed',
+            'progress': f'{group_result.completed_count()}/{len(group_result)}',
+            'results': [res.result for res in group_result.results]
+        })
+    else:
+        return jsonify({
+            'status': 'in_progress',
+            'progress': f'{group_result.completed_count()}/{len(group_result)}'
+        })
 
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
@@ -67,13 +66,11 @@ def subscribe():
     send_weekly_email.apply_async(args=[email])
     return jsonify({'message': 'Subscribed successfully'}), 200
 
-
 @app.route('/unsubscribe', methods=['POST'])
 def unsubscribe():
     email = request.form['email']
     revoke_weekly_email.apply_async(args=[email])
     return jsonify({'message': 'Unsubscribed successfully'}), 200
-
 
 @celery.task
 def blur_image_task(image_path):
