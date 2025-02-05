60cd707d8193d4d23814550bcadab658bc97b928
marina <meliorator50@mail.ru>
2025-02-05 11:32:00 +0300

    temp1

---
+++ module_24_debugging_2/homework/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+# Метрика для отслеживания количества запросов
+counter = metrics.counter(
+    'request_count', 'Количество запросов на эндпоинт', labels={'code': lambda r: r.status_code}
+)
+
+@app.route('/metrics-test')
+@counter
+def test_endpoint():
+    return "Hello, Prometheus!", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
