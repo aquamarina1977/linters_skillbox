a4041d0ade66c7272228337cd2b0dd40b1f50b08
marina <meliorator50@mail.ru>
2025-03-20 13:37:54 +0300

    temp1

---
+++ module_29_testing/hw/main.py
@@ -0,0 +1,5 @@
+from task_2 import create_app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
---
+++ module_29_testing/hw/task_2_db_initialization.py
@@ -0,0 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Инициализация объекта SQLAlchemy без привязки к приложению
+db = SQLAlchemy()
