959633338280c7f832d6dbb928b9d76acfa9d6f2
aquamarina1977 <86408111+aquamarina1977@users.noreply.github.com>
2025-03-24 14:05:12 +0300

    Delete module_14_mvc directory

--- module_14_mvc/homework/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired
-
-class BookForm(FlaskForm):
-    book_title = StringField('Book title', validators=[InputRequired()])
-    author_name = StringField('Author full name', validators=[InputRequired()])
-    submit = SubmitField('Add new book')
